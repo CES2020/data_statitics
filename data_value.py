e3d32e277b95f9ecb986f1497ac6140468147a2c
liyao <nudtliyao@foxmail.com>
2020-03-16 21:41:35 +0800

    优化文件输入输出

--- data_value.py
+++ data_value.py
@@ -11,8 +11,16 @@ import numpy as np
 
 df = pd.read_csv('place_2.csv')
 df = pd.DataFrame(df)
+a = df.drop_duplicates()
+# print(df.info())
+# print(a.info())
+a.to_csv('place_dup.csv', index=False, header=False)
+
+## 统计
 df['value'] = 1
 
 result = df.groupby('name')['value'].sum().sort_values(ascending=False)
 result_json = result.to_json(force_ascii=False, orient='table')
-print(result_json)
+fo = open('name_value.json', 'r+')
+fo.write(result_json)
+#print(result_json)
