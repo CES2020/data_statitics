3ee2c628b55c4c0fd7a817f1e4ac13ce275695e4
liyao <nudtliyao@foxmail.com>
2020-03-16 20:37:31 +0800

    调整输出格式

--- data_value.py
+++ data_value.py
@@ -1,19 +1,18 @@
 #!usr/bin/env python  
-#-*- coding:utf-8 _*-  
+# -*- coding:utf-8 _*-
 """ 
 @author:yaoli 
 @file: data_value.py 
 @time: 2020/03/16 
 """
 
-
 import pandas as pd
 import numpy as np
 
 df = pd.read_csv('place_2.csv')
 df = pd.DataFrame(df)
-df['num'] = 1
+df['value'] = 1
 
-result = df.groupby('小区名称')['num'].sum().sort_values(ascending=False)
-print(result)
-
+result = df.groupby('name')['value'].sum().sort_values(ascending=False)
+result_json = result.to_json(force_ascii=False, orient='table')
+print(result_json)
